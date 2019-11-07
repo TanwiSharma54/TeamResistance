d763e927b8885de9844a0cf2e9cc5c36c487e269
Nhialee Yang <nyang@madisoncollege.edu>
2019-11-06 19:56:02 -0600

    adding test templates

---
+++ src/test/java/com/teamresistance/entity/UserTest.java
@@ -0,0 +1,18 @@
+package com.teamresistance.entity;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+    @Test
+    void getTest() {
+
+
+    }
+}
