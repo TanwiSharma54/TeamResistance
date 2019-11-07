6f76d531ef9888a5c91c9e96818e870457c57be0
Nhialee Yang <nyang@madisoncollege.edu>
2019-11-07 11:24:09 -0600

    updated ZipCodeApi service call

--- src/main/java/com/teamresistance/service/FavoritesService.java
+++ src/main/java/com/teamresistance/service/FavoritesService.java
@@ -14,34 +14,33 @@ public class FavoritesService {
 
     public List<ZipCodesItem> getParkingLotInfo(int zipCode, int radius) throws JsonProcessingException {
 
-        String apiKey = "C6duubd0kQKxJEOmFShO87kDSvVP6U2gpTd65fh7wMBironF9up5xyisynI21Ep9";
+        ZipCodesItem items = new ZipCodesItem();
+        List<ZipCodesItem> list = new ArrayList<>();
+
         String format = "json";
+        String units = "mile";
 
         Client client = ClientBuilder.newClient();
         WebTarget target =
-                client.target("https://www.zipcodeapi.com/rest/"+ apiKey +"/"+radius+".csv/08057/5/miles?minimal");
-                //client.target("https://www.zipcodeapi.com/rest/" + apiKey
-                //        + "/radius." + format + "/"+ zipCode + "/" + radius + "/" + units + "");
 
-        //https://www.zipcodeapi.com/rest/<api_key>/radius.<format>/<zip_code>/<distance>/<units>
+                client.target("https://www.zipcodeapi.com/rest/" + apiKey
+                        + "/radius." + format + "/"+ zipCode + "/" + raidus + "/" + units + "?minimal");
 
-        //if only want zip returned
-        //https://www.zipcodeapi.com/rest/<api_key>/radius.csv/08057/5/miles?minimal
-
-        //specify the type of data to get back
+        //specify the type of data to get back and get the response
         String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
 
-        System.out.println(response);
+        //parse response
+        String[] tokens = response.split("\\t|,|;|\\.|\\?|!|-|:|@|\\[|\\]|\\(|\\)|\\{|\\}|_|\\*|/|\"");
 
-        //map response into a class with jackson library
-        ObjectMapper mapper = new ObjectMapper(); //create mapper to map
+        //grab zipCodes beginning at [i]=7,10,13...
+        for (int i = 7; i < tokens.length; i += 3) {
+            ZipCodesItem item = new ZipCodesItem();
+            item.setZipCode(tokens[i]);
+            list.add(item);
+        }
 
-        //create a list of ZipCode objects
-        ZipCode list = new ZipCode();
-
-        List<ZipCodesItem> zipCodeItems = mapper.readValue(response, new TypeReference<List<ZipCodesItem>>() {});
-
-        return zipCodeItems;
+        //return list of zipCodeItems
+        return list;
     }
 
 }
