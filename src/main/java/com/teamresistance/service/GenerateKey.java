6d0a4acd45f65d44c051af02c576a626d98ac27b
DQueiser <47016589+DQueiser@users.noreply.github.com>
2019-11-20 15:19:14 -0600

    Update GenerateKey.java

--- src/main/java/com/teamresistance/service/GenerateKey.java
+++ src/main/java/com/teamresistance/service/GenerateKey.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.teamresistance.entity.ParkingLot;
+import com.teamresistance.entity.Key;
+import com.teamresistance.persistence.GenericDao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -19,84 +21,36 @@ public class GenerateKey {
     private final Logger logger = LogManager.getLogger(this.getClass());
 
     /**
-     * @param zipCode zipCode to pass in
-     * @param radius radius to pass in
-     * @return parkingLotInfList
-     * @throws JsonProcessingException
+     * @return 20-byte random Key
      */
-    public List<ParkingLot> getParkingLotInfo(int zipCode, int radius) throws JsonProcessingException {
+    public String getRandomKey() {
 
-        String apiKey = "C6duubd0kQKxJEOmFShO87kDSvVP6U2gpTd65fh7wMBironF9up5xyisynI21Ep9";
+        // chose a Character random from this String
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "0123456789"
+                + "abcdefghijklmnopqrstuvxyz";
 
-        //create local variables
-        ZipCodesItem items = new ZipCodesItem();
-        List<ZipCodesItem> list = new ArrayList<ZipCodesItem>();
+        // create StringBuffer size of AlphaNumericString
+        StringBuilder sb = new StringBuilder(20);
 
-        String format = "json";
-        String units = "mile";
+        for (int i = 0; i < 20; i++) {
 
-        Client client = ClientBuilder.newClient();
-        WebTarget target =
-                client.target("https://www.zipcodeapi.com/rest/" + apiKey
-                        + "/radius." + format + "/"+ zipCode + "/" + radius + "/" + units + "?minimal");
+            // generate a random number between
+            // 0 to AlphaNumericString variable length
+            int index
+                    = (int) (AlphaNumericString.length()
+                    * Math.random());
 
-        //specify the type of data to get back and get the response
-        String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
-
-        //parse response
-        String[] tokens = response.split("\\t|,|;|\\.|\\?|!|-|:|@|\\[|\\]|\\(|\\)|\\{|\\}|_|\\*|/|\"");
-
-        //grab zipCodes beginning at [i]=7,10,13...and add to list
-        for (int i = 7; i < tokens.length; i += 3) {
-            ZipCodesItem item = new ZipCodesItem();
-            item.setZipCode(tokens[i]);
-            list.add(item);
+            // add Character one by one in end of sb
+            sb.append(AlphaNumericString
+                    .charAt(index));
         }
+        //Save the generated key for later comparison
+        GenericDao keyDao = new GenericDao(Key.class);
+        Key newKey = new Key();
+        newKey.setAPIKey(sb.toString());
+        int id = keyDao.insert(newKey);
 
-        //pass list of zipcode items to get parkinglot information
-        List<ParkingLot> parkingLotInfoList = getParkingLotInfo(list);
-
-        //return list of parkinglot info
-        return parkingLotInfoList;
+        return sb.toString();
     }
-
-    /**
-     * this method returns a list of parkinglots based on zipcodes
-     * @param list list of zipcodes
-     * @return parkingLotInfo list
-     */
-    public List<ParkingLot> getParkingLotInfo(List<ZipCodesItem> list) {
-
-        List<ParkingLot> parkingInfoList = new ArrayList<>();
-
-        String zip = "";
-
-        for (ZipCodesItem zipCode : list) {
-            zip += zipCode.getZipCode() + ",";
-        }
-
-        try {
-
-            Client client = ClientBuilder.newClient();
-            WebTarget target =
-                    client.target("http://3.130.227.98:8081/parkinglots/" + zip + "");
-
-            //specify the type of data to get back and get the response
-            String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
-
-            System.out.println(response);
-
-            //map response into a class with jackson library
-            ObjectMapper mapper = new ObjectMapper();
-
-            parkingInfoList = mapper.readValue(response, new TypeReference<List<ParkingLot>>() {});
-
-
-        } catch (Exception e) {
-            logger.error("Unable to map object");
-        }
-
-        return parkingInfoList;
-    }
-
 }
