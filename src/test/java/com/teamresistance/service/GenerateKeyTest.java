d49f9259848239304709a06ef9ef68c5fd900157
DQueiser <47016589+DQueiser@users.noreply.github.com>
2019-11-20 15:20:12 -0600

    Update GenerateKeyTest.java

--- src/test/java/com/teamresistance/service/GenerateKeyTest.java
+++ src/test/java/com/teamresistance/service/GenerateKeyTest.java
@@ -1,8 +1,5 @@
 package com.teamresistance.service;
 
-import com.teamresistance.entity.Favorites;
-import com.teamresistance.entity.ParkingLot;
-import com.teamresistance.entity.User;
 import com.teamresistance.persistence.GenericDao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -21,67 +18,15 @@ class GenerateKeyTest {
 
     @BeforeEach
     void setUp() {
-        dao = new GenericDao(Favorites.class);
+        //dao = new GenericDao(Favorites.class);
         Database database = Database.getInstance();
         database.runSQL("cleandb.sql");
     }
 
     @Test
-    void getParkingLotInfo() {
-    }
-
-    @Test
-    void getFavorites() throws Exception {
-        FavoritesService newService = new FavoritesService();
-        List<ParkingLot> places = newService.getParkingLotInfo(53705, 10);
-
-        for (ParkingLot parkingLot : places) {
-            System.out.println(parkingLot.getId());
-        }
-    }
-
-    @Test
-    void getFavorites53094() throws Exception {
-        FavoritesService newService = new FavoritesService();
-        List<ParkingLot> places = newService.getParkingLotInfo(53094, 20);
-
-        for (ParkingLot parkingLot : places) {
-            System.out.println(parkingLot.getId());
-        }
-    }
-
-    @Test
-    void getFavorites53590() throws Exception {
-        FavoritesService newService = new FavoritesService();
-        List<ParkingLot> places = newService.getParkingLotInfo(53590, 10);
-
-        for (ParkingLot parkingLot : places) {
-            System.out.println(parkingLot.getId());
-        }
-    }
-
-    @Test
-    void testInsert() {
-        GenericDao userDao = new GenericDao(User.class);
-        User user = new User("john", "1234");
-        userDao.saveOrUpdate(user);
-
-        User newUser = (User)userDao.getById(3);
-        System.out.println(newUser.getUserName());
-        logger.info(newUser.getUserName());
-
-        int newFaveParkingLotID = 789;
-        String name = "Quick and Out Parking";
-        String description = "2514 W Johnson St, Madison WI 53715";
-        double price = 2.89;
-        int availableLots = 2;
-        Favorites newFavorite = new Favorites(newFaveParkingLotID, name, description, price, availableLots, newUser);
-        user.addFavorites(newFavorite);
-        int id = dao.insert(newFavorite);
-        assertNotEquals(0, id);
-        Favorites insertedFavorite = (Favorites) dao.getById(id);
-        assertNotNull(insertedFavorite);
-        assertEquals(newFaveParkingLotID, insertedFavorite.getParkingLotId());
-
+    void getAPIKey() throws Exception {
+        GenerateKey newKeyService = new GenerateKey();
+        String myKey =  newKeyService.getRandomKey();
+        System.out.println(myKey);
     }
 }
